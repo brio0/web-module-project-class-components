c43c32c080a1e0c68d3dad4fea6f1030d3be772a
Niki Balajadia <niki@Nikis-MacBook-Pro.local>
2022-09-12 14:50:15 -1000

    state made

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,6 +1,25 @@
 import React from 'react'
 
+const todosInitial = [
+  {
+    name: 'Organize Garage',
+    id: 1528817077286, // could look different, you could use a timestamp to generate it
+    completed: false
+  },
+  {
+    name: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  }
+]
+
 export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      todos: todosInitial
+    }
+  }
   render() {
     return (
       <div>
